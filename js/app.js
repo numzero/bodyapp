56e237000a88f4247fa7d102433f117b9ebc2763
numzero <numzero007@hotmail.com>
2014-09-02 19:00:17 +0700

    padding top

--- js/app.js
+++ js/app.js
@@ -138,16 +138,16 @@ app.controller('formCtrl',function($scope,$location,send){
   }
 
   $scope.save=function(){
-   var url="http://lab.cijcorp.com/mkone/server/upload/upload_c1.php"
+   //var url="http://lab.cijcorp.com/mkone/server/upload/upload_c1.php"
    var data = $scope.form;
    console.log(data);
-   send.http(url,"d").success(function(data){
+  /* send.http(url,"d").success(function(data){
     console.log("data");
   }).error(function(data, status, headers, config) {
     console.log("e");
-  });
-
+  }); */
 }
+
 
 $(function(){
   var user_id=window.localStorage['user_id']='540';
@@ -158,158 +158,174 @@ $(function(){
   var temp_url_l2="http://lab.cijcorp.com/mkone/server/upload/upload_l2.php?uid="+user_id;
   var temp_url_l3="http://lab.cijcorp.com/mkone/server/upload/upload_l3.php?uid="+user_id;
   var temp_url_l4="http://lab.cijcorp.com/mkone/server/upload/upload_l4.php?uid="+user_id;
-  /*var img_url="server/temp/";
-  var temp_url_c1="server/upload/upload_c1.php?uid="+user_id;
-  var temp_url_c2="server/upload/upload_c2.php?uid="+user_id;
-  var temp_url_l1="server/upload/upload_l1.php?uid="+user_id;
-  var temp_url_l2="server/upload/upload_l2.php?uid="+user_id;
-  var temp_url_l3="server/upload/upload_l3.php?uid="+user_id;
-  var temp_url_l4="server/upload/upload_l4.php?uid="+user_id;*/
+        /*var img_url="server/temp/";
+        var temp_url_c1="server/upload/upload_c1.php?uid="+user_id;
+        var temp_url_c2="server/upload/upload_c2.php?uid="+user_id;
+        var temp_url_l1="server/upload/upload_l1.php?uid="+user_id;
+        var temp_url_l2="server/upload/upload_l2.php?uid="+user_id;
+        var temp_url_l3="server/upload/upload_l3.php?uid="+user_id;
+        var temp_url_l4="server/upload/upload_l4.php?uid="+user_id;*/
 
-  $("#file_a").on("change",function(){
-    if($("#file_a").val()!=""){
-      $("#prog_a").css('display','block');
-      $("#prog_a").val(0); 
-      $(".img_a").css({'background':"url('/wtf')"});   
+        $("#file_a").on("change",function(){
+          if($("#file_a").val()!=""){
+            $("#prog_a").css('display','block');
+            $("#prog_a").val(0);    
+            $(".img_a").css({'background':"url('')"}); 
+            setTimeout(function () { $(".img_a").css({'background':"rgba(88,88,88,.1)"}); }, 2);
+            $("#file_a").upload(temp_url_c1,function(success){
+              console.log("done",success);
+              if(success!=false){
+                $(".img_a").css('display','none');
+                $("#prog_a").fadeOut();
+                $(".img_a").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
+                $(".img_a").fadeIn();
+              }else{
+                console.log("fail !");
+              }
+            },function(prog,value){
+                    //console.log(value);
+                    $("#prog_a").val(value);
+                  });
+          }else{
+           $("#prog_a").css('display','none');
+         }
+       });
 
-      $("#file_a").upload(temp_url_c1,function(success){
-        console.log("done",success);
-        if(success!=false){
+        $("#file_b").on("change",function(){
+          if($("#file_b").val()!=""){
+            $("#prog_b").css('display','block');
+            $("#prog_b").val(0);    
+            $(".img_b").css({'background':"url('')"}); 
+            setTimeout(function () { $(".img_b").css({'background':"rgba(88,88,88,.1)"}); }, 2);
+            $("#file_b").upload(temp_url_c2,function(success){
+              console.log("done",success);
+              if(success!=false){
+                $(".img_b").css('display','none');
+                $("#prog_b").fadeOut();
+                $(".img_b").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
+                $(".img_b").fadeIn();
+              }else{
+                console.log("fail !");
+              }
+            },function(prog,value){
+                    //console.log(value);
+                    $("#prog_b").val(value);
+                  });
+          }else{
+           $("#prog_b").css('display','none');
+         }
+       });
 
-          $("#prog_a").fadeOut();
-          $(".img_a").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
-        }else{
-          console.log("fail !");
-        }
-      },function(prog,value){
-        //console.log(value);
-        $("#prog_a").val(value);
+
+        $("#file_c").on("change",function(){
+          if($("#file_c").val()!=""){
+            $("#prog_c").css('display','block');
+            $("#prog_c").val(0);    
+            $(".img_c").css({'background':"url('')"}); 
+            setTimeout(function () { $(".img_c").css({'background':"rgba(88,88,88,.1)"}); }, 2);
+            $("#file_c").upload(temp_url_l1,function(success){
+              console.log("done",success);
+              if(success!=false){
+                $(".img_c").css('display','none');
+                $("#prog_c").fadeOut();
+                $(".img_c").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
+                $(".img_c").fadeIn();
+              }else{
+                console.log("fail !");
+              }
+            },function(prog,value){
+                    //console.log(value);
+                    $("#prog_c").val(value);
+                  });
+          }else{
+           $("#prog_c").css('display','none');
+         }
+       });
+
+
+
+        $("#file_d").on("change",function(){
+          if($("#file_d").val()!=""){
+            $("#prog_d").css('display','block');
+            $("#prog_d").val(0);    
+            $(".img_d").css({'background':"url('')"}); 
+            setTimeout(function () { $(".img_d").css({'background':"rgba(88,88,88,.1)"}); }, 2);
+            $("#file_d").upload(temp_url_l2,function(success){
+              console.log("done",success);
+              if(success!=false){
+                $(".img_d").css('display','none');
+                $("#prog_d").fadeOut();
+                $(".img_d").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
+                $(".img_d").fadeIn();
+              }else{
+                console.log("fail !");
+              }
+            },function(prog,value){
+                    //console.log(value);
+                    $("#prog_d").val(value);
+                  });
+          }else{
+           $("#prog_d").css('display','none');
+         }
+       });
+
+
+
+        $("#file_e").on("change",function(){
+          if($("#file_e").val()!=""){
+            $("#prog_e").css('display','block');
+            $("#prog_e").val(0);    
+            $(".img_e").css({'background':"url('')"}); 
+            setTimeout(function () { $(".img_e").css({'background':"rgba(88,88,88,.1)"}); }, 2);
+            $("#file_e").upload(temp_url_l3,function(success){
+              console.log("done",success);
+              if(success!=false){
+                $(".img_e").css('display','none');
+                $("#prog_e").fadeOut();
+                $(".img_e").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
+                $(".img_e").fadeIn();
+              }else{
+                console.log("fail !");
+              }
+            },function(prog,value){
+                    //console.log(value);
+                    $("#prog_e").val(value);
+                  });
+          }else{
+           $("#prog_e").css('display','none');
+         }
+       });
+
+
+        $("#file_f").on("change",function(){
+          if($("#file_f").val()!=""){
+            $("#prog_f").css('display','block');
+            $("#prog_f").val(0);    
+            $(".img_f").css({'background':"url('')"}); 
+            setTimeout(function () { $(".img_f").css({'background':"rgba(88,88,88,.1)"}); }, 2);
+            $("#file_f").upload(temp_url_l4,function(success){
+              console.log("done",success);
+              if(success!=false){
+                $(".img_f").css('display','none');
+                $("#prog_f").fadeOut();
+                $(".img_f").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
+                $(".img_f").fadeIn();
+              }else{
+                console.log("fail !");
+              }
+            },function(prog,value){
+                    //console.log(value);
+                    $("#prog_f").val(value);
+                  });
+          }else{
+           $("#prog_f").css('display','none');
+         }
+       });
+
+
+        //end jquery
       });
-    }else{
-     $("#prog_a").css('display','none');
-   }
- });
-
-  $("#file_b").on("change",function(){
-    if($("#file_b").val()!=""){  
-      $("#prog_b").css('display','block');
-      $("#prog_b").val(0); 
-      $(".img_b").css({'background':"url('/wtf')"}); 
-      $("#file_b").upload(temp_url_c2,function(success){
-        console.log("done",success);
-        if(success!=false){
-
-          $("#prog_b").fadeOut();
-          $(".img_b").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
-        }else{
-          console.log("fail !");
-        }
-      },function(prog,value){
-        //console.log(value);
-        $("#prog_b").val(value);
-      });
-    }else{
-     $("#prog_b").css('display','none');
-   }
- });
-
-  $("#file_c").on("change",function(){
-    if($("#file_c").val()!=""){  
-      $("#prog_c").css('display','block');
-      $("#prog_c").val(0); 
-      $(".img_c").css({'background':"url('/wtf')"}); 
-      $("#file_c").upload(temp_url_l1,function(success){
-        console.log("done",success);
-        if(success!=false){
-
-          $("#prog_c").fadeOut();
-          $(".img_c").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
-        }else{
-          console.log("fail !");
-        }
-      },function(prog,value){
-        //console.log(value);
-        $("#prog_c").val(value);
-      });
-    }else{
-      $("#prog_c").css('display','none');
-    }
-  });
-
-  $("#file_d").on("change",function(){
-    if($("#file_d").val()!=""){  
-      $("#prog_d").css('display','block');
-      $("#prog_d").val(0); 
-      $(".img_d").css({'background':"url('/wtf')"}); 
-      $("#file_d").upload(temp_url_l2,function(success){
-        console.log("done",success);
-        if(success!=false){
-
-          $("#prog_d").fadeOut();
-          $(".img_d").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
-        }else{
-          console.log("fail !");
-        }
-      },function(prog,value){
-        //console.log(value);
-        $("#prog_d").val(value);
-      });
-    }else{
-     $("#prog_d").css('display','none');
-   }
- });
-
-  $("#file_e").on("change",function(){
-    if($("#file_e").val()!=""){  
-      $("#prog_e").css('display','block');
-      $("#prog_e").val(0); 
-      $(".img_e").css({'background':"url('/wtf')"}); 
-      $("#file_e").upload(temp_url_l3,function(success){
-        console.log("done",success);
-        if(success!=false){
-
-          $("#prog_e").fadeOut();
-          $(".img_e").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
-        }else{
-          console.log("fail !");
-        }
-      },function(prog,value){
-        //console.log(value);
-        $("#prog_e").val(value);
-      });
-    }else{
-     $("#prog_e").css('display','none');
-   }
- });
-
-  $("#file_f").on("change",function(){
-    if($("#file_f").val()!=""){  
-      $("#prog_f").css('display','block');
-      $("#prog_f").val(0); 
-      $(".img_f").css({'background':"url('/wtf')"}); 
-      $("#file_f").upload(temp_url_l4,function(success){
-        console.log("done",success);
-        if(success!=false){
-
-          $("#prog_f").fadeOut();
-          $(".img_f").css({'background':"url('"+img_url+success+"') 0 0 no-repeat",'background-size':'200px'});
-        }else{
-          console.log("fail !");
-        }
-      },function(prog,value){
-        //console.log(value);
-        $("#prog_f").val(value);
-      });
-    }else{
-      $("#prog_f").css('display','none');
-    }
-  });
 });
-
-
-
-});
-
 app.controller('upload', function($scope,$http){
   $scope.form={};
   $scope.save=function(){
